e7bbad48e85a70389c67078b03f28f25eb97b009
Tianyang Hu <htyleo@gmail.com>
2015-05-30 18:03:26 +0800

    Update NextPerm

--- LeetCode/NextPerm.java
+++ LeetCode/NextPerm.java
@@ -5,55 +5,55 @@ import java.util.Arrays;
 
 public class NextPerm {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4};
+        int[] a = {1, 2, 3, 4}; // 1 2 4 3
         nextPermutation(a);
         System.out.println(Arrays.toString(a));
         
-        a = new int[]{1, 2, 6, 5, 4, 3};
+        a = new int[]{2, 4, 6, 5, 3, 1}; // 2 5 1 3 4 6
         nextPermutation(a);
         System.out.println(Arrays.toString(a));
         
-        a = new int[]{4, 3, 2, 1};
+        a = new int[]{4, 3, 2, 1}; // 1 2 3 4
         nextPermutation(a);
         System.out.println(Arrays.toString(a));
         
-        a = new int[]{2, 4, 3, 1};
+        a = new int[]{2, 4, 3, 1}; // 3 1 2 4
         nextPermutation(a);
         System.out.println(Arrays.toString(a));
     }
 
     // O(N)
-    // 1. travers from num[N-1] to num[0], find num[i-1] < num[i] (num[i...N-1] is reverse sorted)
-    // 2. swap num[i-1] with smallest number in num[i...N-1] that is larger than num[i-1];
-    // 3. sort num[i...N-1] (simply reverse)
-    public static void nextPermutation(int[] num) {
-        int N = num.length;
+    // 1. traverse from nums[N-1] to nums[0], find nums[i-1] < nums[i] (nums[i...N-1] is reverse sorted)
+    // 2. swap nums[i-1] with smallest numsber in nums[i...N-1] that is larger than nums[i-1];
+    // 3. sort nums[i...N-1] (simply reverse)
+    public static void nextPermutation(int[] nums) {
+        int N = nums.length;
         int i = N - 1;
         for (; i >= 1; i--) {
-            if (num[i-1] < num[i]) {
+            if (nums[i-1] < nums[i]) {
                 break;
             }
         }
         
         if (i != 0) {
             int j = N - 1;
-            for (; j >= i+1; j--) {
-                if (num[j] > num[i-1]) {
+            for (; j > i; j--) {
+                if (nums[j] > nums[i-1]) {
                     break;
                 }
             }
             
-            int tmp = num[i-1];
-            num[i-1] = num[j];
-            num[j] = tmp;
+            int tmp = nums[i-1];
+            nums[i-1] = nums[j];
+            nums[j] = tmp;
         }
         
         int low = i;
         int high = N-1;
         while (low < high) {
-            int tmp = num[low];
-            num[low] = num[high];
-            num[high] = tmp;
+            int tmp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = tmp;
             low++;
             high--;
         }
