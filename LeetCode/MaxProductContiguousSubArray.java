fe45462cda46d8f05a532c505d6ef3875355b928
Tianyang Hu <htyleo@gmail.com>
2015-05-30 17:35:02 +0800

    Update MaxProductContiguousSubArray

--- LeetCode/MaxProductContiguousSubArray.java
+++ LeetCode/MaxProductContiguousSubArray.java
@@ -1,9 +1,11 @@
 // https://oj.leetcode.com/problems/maximum-product-subarray/
 
-public class MaxSubarrayProduct {
+public class MaxProductContiguousSubArray {
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{-4, -3, -2}));
-        System.out.println(maxProduct(new int[]{-1, 0, -2}));
+        System.out.println(maxProduct(new int[]{-4, -3, -2})); // (-4) * (-3) = 12
+        System.out.println(maxProduct(new int[]{-1, 0, -2})); // 0
+        System.out.println(maxProduct(new int[]{-1})); // -1
+        System.out.println(maxProduct(new int[]{1})); // 1
     }
     
     public static int maxProduct(int[] A) {
@@ -13,14 +15,15 @@ public class MaxSubarrayProduct {
         
         for (int i = 1; i < A.length; i++) {
             if (A[i] > 0) {
-                maxTailPos = maxTailPos > 0 ? maxTailPos*A[i] : A[i];
-                minTailNeg = minTailNeg < 0 ? minTailNeg*A[i] : 1;
+                maxTailPos = maxTailPos != -1 ? maxTailPos*A[i] : A[i];
+                minTailNeg = minTailNeg != 1 ? minTailNeg*A[i] : 1;
             } else if (A[i] < 0) {
                 int oriMaxTailPos = maxTailPos;
                 int oriMinTailNeg = minTailNeg;
-                maxTailPos = oriMinTailNeg < 0 ? oriMinTailNeg*A[i] : -1;
-                minTailNeg = oriMaxTailPos > 0 ? oriMaxTailPos*A[i] : A[i];
+                maxTailPos = oriMinTailNeg != 1 ? oriMinTailNeg*A[i] : -1;
+                minTailNeg = oriMaxTailPos != -1 ? oriMaxTailPos*A[i] : A[i];
             } else {
+                // A[i] == 0
                 maxTailPos = -1;
                 minTailNeg = 1;
             }
