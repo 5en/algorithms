f76d1ef92141ec12bfcbff7d61c5d417a3123e78
Tianyang Hu <htyleo@gmail.com>
2015-08-12 17:31:16 +0800

    Update LetterCombinationsOfAPhoneNumber

--- LeetCode/LetterCombinationsOfAPhoneNumber.java
+++ LeetCode/LetterCombinationsOfAPhoneNumber.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+        List<String> result1 = letterCombinations("23");
+        System.out.println(result1);
+
+        letterCombinations2("23");
+    }
+
     public static List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) {
             return new ArrayList<String>();
@@ -39,6 +46,41 @@ public class LetterCombinationsOfAPhoneNumber {
             curr.append(ch);
             dfs(map, digits, start+1, curr, result);
             curr.deleteCharAt(curr.length()-1);
+        }
+    }
+
+    // Just print
+    public static void letterCombinations2(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return;
+        }
+
+        char[][] map = {
+                {}, // 0
+                {}, // 1
+                {'a', 'b', 'c'}, // 2
+                {'d', 'e', 'f'}, // 3
+                {'g', 'h', 'i'}, // 4
+                {'j', 'k', 'l'}, // 5
+                {'m', 'n', 'o'}, // 6
+                {'p', 'q', 'r', 's'}, // 7
+                {'t', 'u', 'v'}, // 8
+                {'w', 'x', 'y', 'z'}, //9
+        };
+
+        dfs2(map, digits, 0, new StringBuilder());
+    }
+
+    private static void dfs2(char[][] map, String digits, int start, StringBuilder curr) {
+        if (start == digits.length()) {
+            System.out.println(curr.toString());
+            return;
+        }
+
+        for (char ch : map[digits.charAt(start) - '0']) {
+            curr.append(ch);
+            dfs2(map, digits, start+1, curr);
+            curr.delete(curr.length()-1, curr.length());
         }
     }
 }
