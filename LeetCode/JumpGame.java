27f387ee78bb6284fa5eb46ba001a2ae8ca1d5fa
Tianyang Hu <htyleo@gmail.com>
2016-02-08 21:45:33 +0800

    Jump Game

---
+++ LeetCode/JumpGame.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/jump-game/
+
+package com.htyleo.algorithms;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int limit = 0;
+        for (int i = 0; i <= limit; i++) {
+            limit = Math.max(limit, i + nums[i]);
+            if (limit >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
