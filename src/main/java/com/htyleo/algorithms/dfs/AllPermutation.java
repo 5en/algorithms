d21102838f1628116d403f5c16e4dadef1f35385
Tianyang Hu <htyleo@gmail.com>
2016-02-10 18:36:06 +0800

    Permutations with duplicates

--- src/main/java/com/htyleo/algorithms/dfs/AllPermutation.java
+++ src/main/java/com/htyleo/algorithms/dfs/AllPermutation.java
@@ -1,7 +1,6 @@
 package com.htyleo.algorithms.dfs;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,96 +9,148 @@ import java.util.Set;
 
 public class AllPermutation {
     public static void main(String[] args) {
-        List<Integer> a = new ArrayList<Integer>(Arrays.asList(new Integer[] { 1, 2, 3 }));
+        int[] nums = { 1, 2, 3 };
 
         System.out.println("perm");
-        for (List<Integer> perm : perm(a)) {
+        for (List<Integer> perm : perm(nums)) {
             System.out.println(perm);
         }
 
         System.out.println("perm with memo");
-        for (List<Integer> perm : permMemo(a)) {
+        for (List<Integer> perm : permMemo(nums)) {
+            System.out.println(perm);
+        }
+
+        int[] nums2 = { 1, 2, 2 };
+        System.out.println("perm with duplicates");
+        for (List<Integer> perm : permDuplicate(nums2)) {
             System.out.println(perm);
         }
     }
 
-    public static Set<List<Integer>> perm(List<Integer> a) {
-        Set<List<Integer>> permSet = new HashSet<List<Integer>>();
-        permSub(a, 0, permSet);
+    public static List<List<Integer>> perm(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<List<Integer>>();
+        permSR(perms, nums, 0);
 
-        return permSet;
+        return perms;
     }
 
-    // a[start, a.size()-1]
-    private static void permSub(List<Integer> a, int start, Set<List<Integer>> permSet) {
-        if (start == a.size() - 1) {
-            permSet.add(new ArrayList<Integer>(a));
+    // nums[start, nums.size()-1]
+    private static void permSR(List<List<Integer>> perms, int[] nums, int start) {
+        if (start == nums.length - 1) {
+            List<Integer> perm = new ArrayList<Integer>();
+            for (int num : nums) {
+                perm.add(num);
+            }
+            perms.add(perm);
             return;
         }
 
-        for (int i = start; i < a.size(); i++) {
-            // swap a[start] and a[i]
-            int tmp = a.get(start);
-            a.set(start, a.get(i));
-            a.set(i, tmp);
+        for (int i = start; i < nums.length; i++) {
+            // swap nums[start] and nums[i]
+            int tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
 
-            permSub(a, start + 1, permSet);
+            permSR(perms, nums, start + 1);
 
             // backtrack
-            tmp = a.get(start);
-            a.set(start, a.get(i));
-            a.set(i, tmp);
+            tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
         }
     }
 
     // with memo
-    public static Set<List<Integer>> permMemo(List<Integer> a) {
+    public static List<List<Integer>> permMemo(int[] nums) {
         // map items to set of its permutations
-        Map<Set<Integer>, Set<List<Integer>>> memo = new HashMap<Set<Integer>, Set<List<Integer>>>();
+        Map<Set<Integer>, List<List<Integer>>> memo = new HashMap<Set<Integer>, List<List<Integer>>>();
 
-        return permMemoSR(a, 0, memo);
+        return permMemoSR(memo, nums, 0);
     }
 
-    // a[start, a.size()-1]
-    private static Set<List<Integer>> permMemoSR(List<Integer> a, int start,
-                                                 Map<Set<Integer>, Set<List<Integer>>> memo) {
-        Set<Integer> key = new HashSet<Integer>(a.subList(start, a.size()));
+    // nums[start, nums.size()-1]
+    private static List<List<Integer>> permMemoSR(Map<Set<Integer>, List<List<Integer>>> memo,
+                                                  int[] nums, int start) {
+        Set<Integer> key = new HashSet<Integer>();
+        for (int i = start; i < nums.length; i++) {
+            key.add(nums[i]);
+        }
 
         if (memo.containsKey(key)) {
-            // found in memo
             return memo.get(key);
         }
 
-        if (start == a.size() - 1) {
+        if (start == nums.length - 1) {
             List<Integer> perm = new ArrayList<Integer>(key);
-            Set<List<Integer>> permSet = new HashSet<List<Integer>>();
-            permSet.add(perm);
-            memo.put(key, permSet);
-
-            return memo.get(key);
+            List<List<Integer>> perms = new ArrayList<List<Integer>>();
+            perms.add(perm);
+            memo.put(key, perms);
+            return perms;
         }
 
-        Set<List<Integer>> permSet = new HashSet<List<Integer>>();
-        for (int i = start; i < a.size(); i++) {
-            // swap a[start] and a[i]
-            int tmp = a.get(start);
-            a.set(start, a.get(i));
-            a.set(i, tmp);
+        List<List<Integer>> perms = new ArrayList<List<Integer>>();
+        for (int i = start; i < nums.length; i++) {
+            // swap nums[start] and nums[i]
+            int tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
 
-            for (List<Integer> subPerm : permMemoSR(a, start + 1, memo)) {
-                List<Integer> perm = new ArrayList<Integer>(a.size() - start);
-                perm.add(a.get(start));
+            for (List<Integer> subPerm : permMemoSR(memo, nums, start + 1)) {
+                List<Integer> perm = new ArrayList<Integer>();
+                perm.add(nums[start]);
                 perm.addAll(subPerm);
-                permSet.add(perm);
+                perms.add(perm);
             }
 
             // backtrack
-            tmp = a.get(start);
-            a.set(start, a.get(i));
-            a.set(i, tmp);
+            tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
         }
-        memo.put(key, permSet);
+        memo.put(key, perms);
 
-        return permSet;
+        return perms;
     }
+
+    // handle duplicates
+    public static List<List<Integer>> permDuplicate(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<List<Integer>>();
+        permuteUniqueSR(perms, nums, 0);
+        return perms;
+    }
+
+    private static void permuteUniqueSR(List<List<Integer>> perms, int[] nums, int start) {
+        if (start == nums.length) {
+            List<Integer> perm = new ArrayList<Integer>();
+            for (int num : nums) {
+                perm.add(num);
+            }
+            perms.add(perm);
+            return;
+        }
+
+        Set<Integer> swaps = new HashSet<Integer>();
+
+        for (int i = start; i < nums.length; i++) {
+            if (swaps.contains(nums[i])) {
+                continue;
+            }
+
+            // swap nums[start] and nums[i]
+            int tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
+
+            swaps.add(nums[start]);
+
+            permuteUniqueSR(perms, nums, start + 1);
+
+            // backtrack
+            tmp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = tmp;
+        }
+    }
+
 }
