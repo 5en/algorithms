de6098f8cd2d2e2c7b302104a0220382e7159a24
Tianyang Hu <htyleo@gmail.com>
2017-06-01 22:51:47 +0800

    format code style

--- LeetCode/FindAllDuplicatesInAnArray.java
+++ LeetCode/FindAllDuplicatesInAnArray.java
@@ -10,32 +10,32 @@ import java.util.List;
  */
 public class FindAllDuplicatesInAnArray {
 
-    public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> res = new ArrayList<>();
+  public List<Integer> findDuplicates(int[] nums) {
+    List<Integer> res = new ArrayList<>();
 
-        // iteratively put num to index num-1
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] - 1 == i) {
-                continue;
-            }
+    // iteratively put num to index num-1
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] - 1 == i) {
+        continue;
+      }
 
-            // set a special value at the starting point nums[i]
-            int num = nums[i];
-            nums[i] = 0;
-            while (num != 0) {
-                if (nums[num - 1] == num) {
-                    res.add(num);
-                    break;
-                }
-
-                int nextNum = nums[num - 1];
-                nums[num - 1] = num;
-                num = nextNum;
-            }
-
+      // set a special value at the starting point nums[i]
+      int num = nums[i];
+      nums[i] = 0;
+      while (num != 0) {
+        if (nums[num - 1] == num) {
+          res.add(num);
+          break;
         }
 
-        return res;
+        int nextNum = nums[num - 1];
+        nums[num - 1] = num;
+        num = nextNum;
+      }
+
     }
+
+    return res;
+  }
 
 }
--- LeetCode/FindAllNumbersDisappearedInAnArray.java
+++ LeetCode/FindAllNumbersDisappearedInAnArray.java
@@ -10,35 +10,35 @@ import java.util.List;
  */
 public class FindAllNumbersDisappearedInAnArray {
 
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> res = new ArrayList<>();
+  public List<Integer> findDisappearedNumbers(int[] nums) {
+    List<Integer> res = new ArrayList<>();
 
-        // iteratively put num to index num-1
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] - 1 == i) {
-                continue;
-            }
+    // iteratively put num to index num-1
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] - 1 == i) {
+        continue;
+      }
 
-            int num = nums[i];
-            nums[i] = 0;
-            while (num != 0) {
-                if (nums[num - 1] == num) {
-                    break;
-                }
-
-                int nextNum = nums[num - 1];
-                nums[num - 1] = num;
-                num = nextNum;
-            }
+      int num = nums[i];
+      nums[i] = 0;
+      while (num != 0) {
+        if (nums[num - 1] == num) {
+          break;
         }
 
-        for (int i = 1; i <= nums.length; i++) {
-            if (nums[i - 1] != i) {
-                res.add(i);
-            }
-        }
-
-        return res;
+        int nextNum = nums[num - 1];
+        nums[num - 1] = num;
+        num = nextNum;
+      }
     }
+
+    for (int i = 1; i <= nums.length; i++) {
+      if (nums[i - 1] != i) {
+        res.add(i);
+      }
+    }
+
+    return res;
+  }
 
 }
