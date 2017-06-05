de076ec7ae13792d53db4d04331d09a61444acfd
Tianyang Hu <htyleo@gmail.com>
2017-06-05 21:37:38 +0800

    462. Minimum Moves to Equal Array Elements II

--- LeetCode/MinimumMovesToEqualArrayElementsII.java
+++ LeetCode/MinimumMovesToEqualArrayElementsII.java
@@ -1,23 +1,70 @@
-// https://leetcode.com/contest/leetcode-weekly-contest-10/problems/minimum-moves-to-equal-array-elements-ii/
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/#/description
+
+package com.htyleo.algorithms;
 
 import java.util.Arrays;
 
+/**
+ * Created by htyleo on 6/5/17.
+ */
 public class MinimumMovesToEqualArrayElementsII {
 
-    // move to the median
-    public int minMoves2(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        Arrays.sort(nums);
-
-        int result = 0;
-        for (int low = 0, high = nums.length - 1; low < high; low++, high--) {
-            result += nums[high] - nums[low];
-        }
-
-        return result;
+  // O(N) time, quick select
+  public int minMoves2(int[] nums) {
+    // find median
+    int median = findNth(nums, (nums.length + 1) / 2, 0, nums.length - 1);
+    int move = 0;
+    for (int num : nums) {
+      move += Math.abs(num - median);
     }
 
+    return move;
+  }
+
+  private int findNth(int[] nums, int n, int start, int end) {
+    if (end - start <= 1) {
+      return n == 1 ? Math.min(nums[start], nums[end]) : Math.max(nums[start], nums[end]);
+    }
+
+    int pivotIdx = (start + end) / 2;
+    int pivot = nums[pivotIdx];
+
+    swap(nums, pivotIdx, end);
+
+    pivotIdx = start;
+    for (int i = start; i <= end - 1; i++) {
+      if (nums[i] < pivot) {
+        swap(nums, i, pivotIdx);
+        pivotIdx++;
+      }
+    }
+
+    swap(nums, pivotIdx, end);
+
+    if (pivotIdx - start + 1 > n) {
+      return findNth(nums, n, start, pivotIdx - 1);
+    } else if (pivotIdx - start + 1 < n) {
+      return findNth(nums, n - (pivotIdx - start + 1), pivotIdx + 1, end);
+    } else {
+      return pivot;
+    }
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+
+  // O(NlogN) time
+  public int minMoves2NlogN(int[] nums) {
+    int move = 0;
+
+    Arrays.sort(nums);
+    for (int lo = 0, hi = nums.length - 1; lo < hi; lo++, hi--) {
+      move += nums[hi] - nums[lo];
+    }
+
+    return move;
+  }
 }
